829849905d774d815dda35c9e7eb7c8468e7481b
NelinVitaliy <s5001481.nelin@gmail.com>
2019-11-16 15:04:05 +0600

    Chapter_2__Task_8

---
+++ Task_8.py
@@ -0,0 +1,11 @@
+print('Type an integer number 1: ')
+a = int(input())
+print('Type an integer number 2: ')
+b = int(input())
+for a in range(a, b+1):
+    print(a, end=" ")
+
+
+# a = int(input())
+# b = int(input())
+# print(list(range(a, b+1)))
